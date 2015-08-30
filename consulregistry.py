a630bd076a45b028a762d0a25ca1ee5b7f7511c9
Anthony Lapenna <lapenna.anthony@gmail.com>
2015-08-30 13:46:08 +0200

    Use consul SDK. Close #12

---
+++ consulregistry.py
@@ -0,0 +1,25 @@
+import requests
+import consul
+
+
+class ConsulRegistry:
+
+    def __init__(self, host, port):
+        self.client = consul.Consul(host=host, port=port)
+
+    def get_value_for_key(self, key):
+        index, data = self.client.kv.get(key)
+        value = None
+        if data is None:
+            print('Unable to retrieve value for key: {}'.format(key))
+        else:
+            value = data["Value"]
+        return value
+
+    def get_services(self, service_prefix):
+        managed_services = []
+        index, services = self.client.catalog.services()
+        for key, value in services.items():
+            if key.startswith(service_prefix):
+                managed_services.append(key)
+        return managed_services
